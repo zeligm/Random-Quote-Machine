61327a99e21f222eedabe6adf3089372dd31b111
Zelig Malamud <zeligmalamud@gmail.com>
2016-06-13 22:43:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
 var quoteRandomizer = {
   quotes:[
             {
-              quote: "Every day it gets a little easier. But you have to do it every day, that’s the hard part. But it does gets easier.",
+              quote: "<strong><em>Every day it gets a little easier. But you have to do it every day, that’s the hard part. But it does gets easier.</strong></em>",
             },
             {
-              quote: "If you’re going through hell, keep going.",
+              quote: "<strong><em>If you’re going through hell, keep going.</strong></em>",
             },
             {
-              quote: "It doesn't take talent to practice.",
+              quote: "<strong><em>It doesn't take talent to practice.</strong></em>",
             },
             {
-              quote: "Don’t watch the clock, do what it does. Keep going.",
+              quote: "<strong><em>Don’t watch the clock, do what it does. Keep going.</strong></em>",
             },
             {
-              quote: "The first and greatest victory is to conquer yourself. To be conquered by yourself is of all things most shameful and vile.",
+              quote: "<strong><em>The first and greatest victory is to conquer yourself. To be conquered by yourself is of all things most shameful and vile.</strong></em>",
             },
             {
-              quote: "Motivation is fleeting, discipline is not.",
+              quote: "<strong><em>Motivation is fleeting, discipline is not.</strong></em>",
             },
             {
-              quote: "Pain is inevitable, suffering is optional.",
+              quote: "<strong><em>Pain is inevitable, suffering is optional.</strong></em>",
             },
             {
-              quote: "The master has failed more times than the beginner has even tried.",
+              quote: "<strong><em>The master has failed more times than the beginner has even tried.</strong></em>",
             },
             {
-              quote: "If you want to be good at something you first must be willing to be bad at it.",
+              quote: "<strong><em>If you want to be good at something you first must be willing to be bad at it.</strong></em>",
             },
         ],
   randomize: function () {
